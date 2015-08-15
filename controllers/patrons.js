32cd2028809ffc2ebf57e33be6b720e27d27d504
merittrompeter <mktrompeter@gmail.com>
2015-08-15 12:59:53 -0700

    simple views, some validations

--- controllers/patrons.js
+++ controllers/patrons.js
@@ -20,11 +20,27 @@ router.get("/login", function(req, res){
 
 // Post Login
 router.post("/login", function(req, res){
-        res.render('plates/new');
+
+             db.patron.authenticate(req.body.email,req.body.password,function(err,patron){
+                if(err){
+                        res.send(err);
+                }else if(patron){
+                        req.session.patron = patron.id;
+                        req.flash('success','Welcome Chef.');
+                        res.redirect('plates/new');
+                }else{
+                        req.flash('danger',"Sorry, we don't recognize that username and/or password");
+                        res.redirect('patrons/login');
+                }
+                });
+
 });
 
 // Post Logout
 router.post("logout", function(req, res){
+        req.flash('info','Thanks, patron. Until we dine again!.');
+          req.session.user = false;
+          res.redirect('/');
 });
 
 module.exports = router;
--- models/patron.js
+++ models/patron.js
@@ -1,13 +1,57 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var patron = sequelize.define('patron', {
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    phone: DataTypes.STRING
+email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail:true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len:[4,12],
+        notEmpty: true
+      }
+    },
+    phone: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true
+      }
+    }
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+      },
+      authenticate: function(email,password,callback){
+        this.find({where:{email:email}}).then(function(user){
+          if(user){
+            bcrypt.compare(password,user.password,function(error,result){
+              if(error){
+                callback(error);
+              }else{
+                callback(null, result ? user : false);
+              }
+            });
+          }else{
+            callback(null, false);
+          }
+        }).catch(callback);
+      }
+    },
+    hooks: {
+      beforeCreate: function(user, options, callback){
+        if(user.password){
+          bcrypt.hash(user.password,10,function(error,hash){
+            if(error) return callback(error);
+            user.password = hash;
+            callback(null, user);
+          });
+        }else{
+          callback(null, user);
+        }
       }
     }
   });
